0a5561c8e44ec8c47eb055a767354d96235a6ef3
leegunwoooo <leegunwoo0325@gmail.com>
2025-02-23 00:12:47 +0900

    first

---
+++ src/main/java/kick/kickdeal/KickdealApplication.java
@@ -0,0 +1,13 @@
+package kick.kickdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickdealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickdealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kick/kickdeal/KickdealApplicationTests.java
@@ -0,0 +1,13 @@
+package kick.kickdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickdealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
