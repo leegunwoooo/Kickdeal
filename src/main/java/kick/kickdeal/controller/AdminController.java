f8854260c565147ae74c01775ef3c6760277446d
leegunwoooo <leegunwoo0325@gmail.com>
2025-03-05 17:56:35 +0900

    테스트용 admin 컨트롤러

---
+++ src/main/java/kick/kickdeal/controller/AdminController.java
@@ -0,0 +1,29 @@
+package kick.kickdeal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public Object ok() {
+        return new Response("OK");
+    }
+
+    static class Response {
+        private String message;
+
+        public Response(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
