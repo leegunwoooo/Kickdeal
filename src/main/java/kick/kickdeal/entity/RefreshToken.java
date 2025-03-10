d7b775e6b7441101f81e09921b0fe1bb3ddad2d3
leegunwoooo <leegunwoo0325@gmail.com>
2025-03-10 17:37:37 +0900

    FEAT :: username에 유니크 속성 추가

--- src/main/java/kick/kickdeal/entity/RefreshToken.java
+++ src/main/java/kick/kickdeal/entity/RefreshToken.java
@@ -16,7 +16,8 @@ public class RefreshToken {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false, unique = true)
