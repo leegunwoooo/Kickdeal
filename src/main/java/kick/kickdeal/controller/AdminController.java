970a630eb7cd8367f629c0042e1f611796c2c67f
leegunwoooo <leegunwoo0325@gmail.com>
2025-03-12 00:11:53 +0900

    FEAT :: 어드민으로 전환 가능한 컨트롤러 추가

--- src/main/java/kick/kickdeal/controller/AdminController.java
+++ src/main/java/kick/kickdeal/controller/AdminController.java
@@ -1,29 +1,27 @@
 package kick.kickdeal.controller;
 
+import kick.kickdeal.entity.Role;
+import kick.kickdeal.entity.User;
+import kick.kickdeal.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AdminController {
 
-    @GetMapping("/admin")
-    public Object ok() {
-        return new Response("OK");
-    }
+    private final UserRepository userRepository;
 
-    static class Response {
-        private String message;
+    @PutMapping("/admin")
+    public User updateAdmin(@RequestBody String Username){
+        User user = userRepository.findByName(Username)
+                .orElseThrow(() -> new IllegalArgumentException("없는 사용자입니다."));
 
-        public Response(String message) {
-            this.message = message;
-        }
+        user.setRole(Role.ROLE_ADMIN);
 
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
+        return user;
     }
 }
