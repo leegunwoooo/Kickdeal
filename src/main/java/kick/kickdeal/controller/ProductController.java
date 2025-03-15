44573afd9e6d142ac7d7d769ccb210a8094abe82
구현우 <hyunwoo@guhyeon-uui-MacBookAir.local>
2025-03-15 17:06:58 +0900

    DELETE :: application.yml의 default-bucket

--- src/main/java/kick/kickdeal/controller/ProductController.java
+++ src/main/java/kick/kickdeal/controller/ProductController.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.awt.*;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
